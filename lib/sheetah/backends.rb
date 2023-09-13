d6d9321369dc0f0cfac3a7e6972123799d6a6119
Erwan Thomas <erwan.thomas@steeple.fr>
2023-09-13 23:06:08 +0200

    Remove backends auto-detection
    Even though it might be convenient for users not to have to worry about
    which backend should handle their input, the way it was implemented was
    not deterministic, and actually error-prone.
    
    By the way, auto-detection isn't actually such a big deal for the
    library itself, users can add their own helpers on top of the library,
    using with their own rules.
    
    So, instead of spending time on a not-so-valuable effort to find a
    better implementation, the best compromise is to remove the feature
    altogether and keep things simple.

--- lib/sheetah/backends.rb
+++ lib/sheetah/backends.rb
@@ -1,25 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "backends_registry"
-require_relative "messaging/messages/no_applicable_backend"
-require_relative "utils/monadic_result"
-
 module Sheetah
   module Backends
-    @registry = BackendsRegistry.new
-
     class << self
-      attr_reader :registry
-
-      def open(*args, **opts, &block)
-        backend = opts.delete(:backend) || registry.get(*args, **opts)
-
-        if backend.nil?
-          return Utils::MonadicResult::Failure.new(
-            Messaging::Messages::NoApplicableBackend.new
-          )
-        end
-
+      def open(*args, backend:, **opts, &block)
         backend.open(*args, **opts, &block)
       end
     end
--- spec/sheetah/backends_spec.rb
+++ spec/sheetah/backends_spec.rb
@@ -3,12 +3,6 @@
 require "sheetah/backends"
 
 RSpec.describe Sheetah::Backends do
-  describe "::registry" do
-    it "is a registry of backends" do
-      expect(described_class.registry).to be_a(Sheetah::BackendsRegistry)
-    end
-  end
-
   describe "::open" do
     let(:backend) do
       double
@@ -18,26 +12,16 @@ RSpec.describe Sheetah::Backends do
     let(:bar) { double }
     let(:res) { double }
 
-    it "may open with an explicit backend" do
+    it "opens with a backend" do
       allow(backend).to receive(:open).with(foo, bar: bar).and_return(res)
-      expect(described_class.registry).not_to receive(:get)
 
       expect(described_class.open(foo, backend: backend, bar: bar)).to be(res)
     end
 
-    it "may open with an implicit backend" do
-      allow(backend).to receive(:open).with(foo, bar: bar).and_return(res)
-      allow(described_class.registry).to receive(:get).with(foo, bar: bar).and_return(backend)
-
-      expect(described_class.open(foo, bar: bar)).to be(res)
-    end
-
-    it "may miss a backend to open" do
-      allow(described_class.registry).to receive(:get).with(foo, bar: bar).and_return(nil)
-
-      result = described_class.open(foo, bar: bar)
-      expect(result).to be_failure
-      expect(result.failure).to eq(Sheetah::Messaging::Messages::NoApplicableBackend.new)
+    it "may not open without a backend" do
+      expect { described_class.open(foo, bar: bar) }.to raise_error(
+        ArgumentError, /missing keyword: :backend/
+      )
     end
   end
 end
