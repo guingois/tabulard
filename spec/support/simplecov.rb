2f81e705a9bf8510a208b0d3a13b8fe15542948c
Erwan Thomas <id@maen.fr>
2024-06-03 12:56:06 +0200

    Draft CI reports to GitHub & CodeClimate

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
-return unless ENV.fetch("COVERAGE", nil) == "true"
+formatters = []
+
+if ENV["COVERAGE_REPORT_HTML"] == "true"
+  formatters << "html"
+end
+
+if ENV["COVERAGE_REPORT_COBERTURA"] == "true"
+  formatters << "cobertura"
+end
+
+return if formatters.empty?
 
 require "simplecov"
+
+formatters.map! do |formatter|
+  case formatter
+  when "html"
+    SimpleCov::Formatter::HTMLFormatter
+  when "cobertura"
+    require "simplecov-cobertura"
+    SimpleCov::Formatter::CoberturaFormatter
+  else
+    raise "Unknown SimpleCov formatter: #{formatter}"
+  end
+end
+
+SimpleCov.formatters = formatters
 
 SimpleCov.start do
   coverage_dir "docs/coverage"
