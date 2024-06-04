1c46111c0b2b4d315f88413521249ab04b62ce44
Erwan Thomas <id@maen.fr>
2024-06-04 15:17:26 +0200

    foo

---
+++ lib/foo.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Foo
+  def foo
+    puts "foo"
+  end
+end
---
+++ spec/foo_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe "Foo" do
+  it "fails" do
+    expect(described_class).to be(false)
+  end
+end
