73c067cb4cceadf1a09ee89a4aa8e82fed5f381c
Erwan Thomas <id@maen.fr>
2024-06-11 11:18:05 +0200

    Wrapper: streamline data access before changes

--- lib/sheetah/backends/wrapper.rb
+++ lib/sheetah/backends/wrapper.rb
@@ -11,13 +11,9 @@ module Sheetah
         super(**opts)
 
         if (table_size = table.size).positive?
-          @table      = table
-          @headers    = @table[0]
-          @rows_count = table_size - 1
-          @cols_count = @headers.size
+          init_with_filled_table(table, table_size: table_size)
         else
-          @rows_count = 0
-          @cols_count = 0
+          init_with_empty_table
         end
       end
 
@@ -26,8 +22,10 @@ module Sheetah
 
         return to_enum(:each_header) { @cols_count } unless block_given?
 
-        1.upto(@cols_count) do |col|
-          yield Header.new(col: Sheet.int2col(col), value: @headers[col - 1])
+        @cols_count.times do |col_index|
+          col, cell_value = read_cell(@headers, col_index)
+
+          yield Header.new(col: col, value: cell_value)
         end
 
         self
@@ -38,17 +36,55 @@ module Sheetah
 
         return to_enum(:each_row) unless block_given?
 
-        1.upto(@rows_count) do |row|
-          raw = @table[row]
+        @rows_count.times do |row_index|
+          row, row_data = read_row(row_index)
 
-          value = Array.new(@cols_count) do |col_idx|
-            Cell.new(row: row, col: Sheet.int2col(col_idx + 1), value: raw[col_idx])
+          row_value = Array.new(@cols_count) do |col_index|
+            col, cell_value = read_cell(row_data, col_index)
+
+            Cell.new(row: row, col: col, value: cell_value)
           end
 
-          yield Row.new(row: row, value: value)
+          yield Row.new(row: row, value: row_value)
         end
 
         self
+      end
+
+      private
+
+      def init_with_filled_table(table, table_size:)
+        @table = table
+
+        headers_row = 0
+        @headers = table[headers_row]
+
+        @first_row = headers_row.succ
+        @first_row_name = 1
+        @rows_count = table_size - @first_row
+
+        @first_col = 0
+        @first_col_name = 1
+        @cols_count = @headers.size
+      end
+
+      def init_with_empty_table
+        @rows_count = 0
+        @cols_count = 0
+      end
+
+      def read_row(row_index)
+        row = @first_row_name + row_index
+        row_data = @table[@first_row + row_index]
+
+        [row, row_data]
+      end
+
+      def read_cell(row_data, col_index)
+        col = Sheet.int2col(@first_col_name + col_index)
+        cell_value = row_data[@first_col + col_index]
+
+        [col, cell_value]
       end
     end
   end
